20611f5be041c69975dfd1e04765452b7d303b84
GonzaOxo <gonzawarcry@gmail.com>
2021-07-14 15:06:58 -0300

    santo padre v2.1

--- docker/app.js
+++ docker/app.js
@@ -29,7 +29,21 @@ const getRandomMessage = () =>{
          "ME ESGUILSE!",
          "SALUDOS A TODOS LOS PITOS",
          "TE GUSTAN LOS PEBETES LEA!",
-         "ETZELENTE!"];
+         "ETZELENTE!",
+		 "CUIDATE LA COLA CHABON",
+         "NO SE COMAN A MI HERMANA CHABON"];
+    return messages[Math.floor(Math.random()*messages.length)];
+}
+
+const getRandomLyrics = () =>{
+    let messages =
+        ["Es un angel que va cabalgando cabalgando con brio y valor va cantando las tristes historias de una guerra que ya terminó.",
+		 "primavera lejos de mi patria primavera lejos de mi amor primavera sin flores y sin risas primavera a orillas del volchow",
+         "y sus aguas que dan al ladoga van cantando esta triste cancion, cancion triste de amor y de guerra cancion triste de guerra y amor.",
+         "cuando ebrio ataca el enemigo y con vodka ataca sin valor rasga el aire mas fuerte que la metralla las estrofas de mi cara al sol.",
+         "cara al sol cancion antigua y nueva cara al sol es el himno mejor cara al sol y morir peleando que mi patria asi me lo pidio.",
+         "si en la lucha yo quedara roto marcharia en la legion de honor montaria en la guardia de los luceros",
+		 "formaria junto al mejor montaria en la guardia de los luceros formaria junto al mejor"];
     return messages[Math.floor(Math.random()*messages.length)];
 }
 
@@ -49,6 +63,11 @@ const contarChiste = (msg) =>{
     });
 }
 
+const letraMusica = (msg) =>{
+	bot.sendMessage(msg.chat.id,"ESCUCHATE ESTA HAHAHA SOY ITALIANO " + getRandomLyrics());
+};
+
+
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
@@ -59,6 +78,11 @@ bot.on('text', (msg) => {
     bot.sendMessage(chatId, 'CHABON mi nombre es BERZERKER');
   }
 
+  if (msg.text.toString().toLowerCase().includes("feliz cumple")) {
+    let username = msg.from.first_name || "";
+    bot.sendMessage(chatId, 'FELIZ CUMPLE '+ username + "NO TE COMAS MUCHOS PIBES HAHAHA");
+  }
+
   if (msg.text.toString().toLowerCase().includes("bambino")) {
     bot.sendMessage(chatId, 'HAHAHA. Etzelente.');
   }
@@ -66,15 +90,26 @@ bot.on('text', (msg) => {
   if (msg.text.toString().toLowerCase().includes("pizza")) {
     bot.sendMessage(chatId, 'Quiero comer pitza HAHAHa.');
   }
-  
-  if (msg.text.toString().toLowerCase().includes("pini")) {
+
+  /*if (msg.text.toString().toLowerCase().includes("pini")) {
     bot.sendMessage(chatId, 'Che y si volvemos a WhatsApp?');
-  }
-  
+  }*/
+
   if (msg.text.toString().toLowerCase().includes("chiste")) {
     contarChiste(msg);
   }
-  
+
+  if (msg.text.toString().toLowerCase().includes("callate gordo")) {
+    bot.sendMessage(chatId, 'NO SOY GORDO SOY PURO MACHO CHABON HAHAHA BAMBINO.');
+  }
+
+  if (msg.text.toString().toLowerCase().includes("nos juntamos")) {
+    bot.sendMessage(chatId, 'LOS PASO A BUSCAR CON LA CUMBANCHA VIENE EL BILLY Y SU METRALLA HAHAHA');
+  }
+
+  if (msg.text.toString().toLowerCase().includes("compre")) {
+    bot.sendMessage(chatId, 'CHABON YO TENGO UNO MEJOR EN MI CASA MAS GRANDE Y MAS LINDO');
+  }
 });
 
 bot.on('new_chat_members', (msg) => {
@@ -93,7 +128,7 @@ bot.onText(/^\/clima/, function(msg, match){
 
     var opciones = {
         search: ciudad,
-        degreeType: 'C', 
+        degreeType: 'C',
         lang: 'es-ES'
     }
 
@@ -104,7 +139,7 @@ bot.onText(/^\/clima/, function(msg, match){
             console.log(result[0]);
 
             if( result[0].current.temperature <= 16) {
-                comentario = "CHABON hoy vamos a tener un dia fresco, por la mañana\n\n"
+                comentario = "CHABON hoy vamos a tener un dia fresco\n\n"
             }
             else if( 16 < result[0].current.temperature < 27) {
                 comentario = "CHABON hoy vamos a tener un dia calido\n\n"
@@ -112,7 +147,7 @@ bot.onText(/^\/clima/, function(msg, match){
             else {
                 comentario = "Hoy va ser un dia muy caluroso. CHABON\n\n"
             }
-            
+
             bot.sendMessage(chatId, comentario + "Lugar: " + result[0].location.name +
             "\n\nTemperatura: " + result[0].current.temperature + "ºC\n" +
             "Visibilidad: " + result[0].current.skytext + "\n" +
@@ -125,4 +160,8 @@ bot.onText(/^\/clima/, function(msg, match){
 
 bot.onText(/^\/dado/, (msg) => {
     bot.sendDice(msg.chat.id);
+});
+
+bot.onText(/^\/musica/, function(msg, match){
+    letraMusica(msg);
 });
